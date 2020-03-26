62b1dc066cea6ebdb85d141ee07244366c796162
Bruno <b-coimbra@hotmail.com>
2020-03-26 20:26:22 -0300

    added wacom script

---
+++ scripts/wacom.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+## Stylus
+xsetwacom set "Wacom Intuos S Pen stylus" MapToOutput eDP-1
+xsetwacom set "Wacom Intuos S Pen stylus" Area 0 5400 5500 9200
+# xsetwacom set "Wacom Intuos S Pen stylus" Area 0 4600 7360 9200
+xsetwacom set "Wacom Intuos S Pen stylus" PressureCurve 0 0 100 100
+
