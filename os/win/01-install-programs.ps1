4057d73d935d1418a0dd6a7a2c6578586c958046
0-l <b-coimbra@hotmail.com>
2022-03-14 22:04:28 -0300

    add windows scripts

---
+++ os/win/01-install-programs.ps1
@@ -0,0 +1,11 @@
+# browser
+choco install vivaldi -y
+
+# programs
+choco install discord qbittorrent stremio -y
+
+# dev tools/lang
+choco install git emacs cmder nodejs dotnet python3 -y
+
+# tools
+choco install quicklook 7zip autohotkey megasync sharex -y
